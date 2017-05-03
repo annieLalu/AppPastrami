716f771ef07ac12d3fd031751c78991d5b73076c
annieLalu <annalisa.filippi@outlook.it>
2017-05-03 18:34:43 +0200

    first commit for app Pastrami

---
+++ Pastrami/app/src/main/java/it/annalisa/pastrami/ListaPanini.java
@@ -0,0 +1,28 @@
+package it.annalisa.pastrami;
+
+/**
+ * Created by Annalisa on 03/05/2017.
+ */
+public class ListaPanini{
+
+    int red_peppered;
+    int black_peppered;
+    int corned_beef;
+    int chicken;
+    int red_pepperedXL;
+    int black_pepperedXL;
+    int corned_beefXL;
+    int chickenXL;
+
+   public ListaPanini(){
+       red_peppered = 0;
+       black_peppered = 0;
+       corned_beef = 0;
+       chicken = 0;
+       red_pepperedXL = 0;
+       black_pepperedXL = 0;
+       corned_beefXL = 0;
+       chickenXL = 0;
+    }
+
+}
